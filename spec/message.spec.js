23497531711b503d5dc7213e8fa94b41b68c2ca7
Edward King <king8three@gmailcom>
2024-07-21 23:24:29 -0400

    First 6 Tests Passing

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -5,5 +5,26 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Message class", function() {
+   
+        //TEST 4
+        it("throws error if name is NOT passed into constructor as the first parameter", function() {
+          expect( function() { new Message();}).toThrow(new Error('Name required.'));
+      
+        });
+        //TEST 5
+        it("constructor sets name", function() {
+          let newObjectName = new Message("Message")
+          expect(newObjectName.name).toBe("Message")
+        });
+        //TEST 6
+        it("contains a commands array passed into the constructor as the 2nd argument", function() {
 
-});
+        let commands = ["hello"]    
+        let newObjectCommandsArray = new Message("Message", commands)
+        expect(newObjectCommandsArray.commands).toBe(commands)
+        });  
+      
+      });
+      
+
+
