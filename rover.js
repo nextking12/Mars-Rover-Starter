8abcd64d66c63a8d018dcae43289a6ca99a067ed
Edward King <king8three@gmailcom>
2024-07-22 22:08:22 -0400

    8 tests passing

--- rover.js
+++ rover.js
@@ -1,10 +1,31 @@
+const Message = require("./message");
+
 class Rover {
    // Write code here!
    constructor(position){
       this.position = position;
       this.mode = "NORMAL";
       this.generatorWatts = 110
+      
+      }
+      receieveMessage(message){
+         let retObj = {};
+
+         if (message.name){
+            retObj["name"] = message.name
+            //console.log(retObj);
+            return retObj;
+         }
+        // return retObj;
+
+
+      }
    }
-}
+   
+      
+      
+   
+
+
 
 module.exports = Rover;
